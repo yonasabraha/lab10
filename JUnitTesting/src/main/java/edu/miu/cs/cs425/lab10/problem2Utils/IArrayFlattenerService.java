9328c1005fd85136ba189b4dc80cbad1264a0e0a
yonasabraha <84365310+yonasabraha@users.noreply.github.com>
2021-07-10 09:09:00 -0500

    first commit

---
+++ JUnitTesting/src/main/java/edu/miu/cs/cs425/lab10/problem2Utils/IArrayFlattenerService.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs425.lab10.problem2Utils;
+
+public interface IArrayFlattenerService {
+
+    int[] flattenArray(int[][] input);
+
+}
---
+++ JUnitTesting/src/test/java/edu/miu/cs/cs425/lab10/problem2Utils/ArrayReversorTestCases.java
@@ -0,0 +1,9 @@
+package edu.miu.cs.cs425.lab10.problem2Utils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ArrayReversorTest.class,ArrayReversorNullTest.class})
+public class ArrayReversorTestCases {
+}
